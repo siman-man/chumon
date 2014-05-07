00d83982efd88f766c1815a7ada5794a6d9fab83
siman-man <k128585@ie.u-ryukyu.ac.jp>
2014-05-07 09:36:33 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chumon::Application.config.secret_key_base = '0956695b4566725438b94afe7a5b2176c1769b3172d34dbe1647ba9148b25fc32df890a2805c4cec4b0b2fac7fc02695217ecfde416e26c02496374b76890e93'
