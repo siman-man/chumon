4cbc766fb5001524f6007705bd8cf41e0748a44a
siman-man <k128585@ie.u-ryukyu.ac.jp>
2014-05-07 12:01:58 +0900

    Add title for each static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Chumon::Application.routes.draw do
-  #get "static_pages/home"
-  get "static_pages/agreement" => 'static_pages#help'
+  get "static_pages/home"
+  get "static_pages/agreement" 
   get "static_pages/help"
-  get 'static_pages/home' => 'static_pages#agreement'
   resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
