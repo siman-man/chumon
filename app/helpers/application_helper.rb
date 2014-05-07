9edac778767d6210eeccf4572b5c4578dbf2d5d7
siman-man <k128585@ie.u-ryukyu.ac.jp>
2014-05-07 14:00:58 +0900

    Practice3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base = 'Chumon'
+
+    if page_title.empty?
+      base
+    else
+      "#{base} | #{page_title}"
+    end
+  end
+
+  def view_nabeatsu(count)
+    list = []
+    list << "<ul>"
+
+    1.upto(count) do |number|
+      list << if number % 3 == 0 || number.to_s =~ /3/
+        "<li>aho</li>"
+      else
+        "<li>#{number}</li>"
+      end
+    end
+
+    list << "</ul>"
+    list.join("\n")
+  end
 end
