16bd30e265c7be0f628a73b6d415c22128deb510
siman-man <k128585@ie.u-ryukyu.ac.jp>
2014-05-08 10:52:36 +0900

    Finish practice

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,15 @@ class Product < ActiveRecord::Base
 
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 100 }
   validates :name, length: { minimum: 2, maximum: 100 }
+
+  
+  def sales_count
+    order_details.sum(:count)
+  end
+
+  class << self
+    def sales_ranking(limit = 10)
+      all.sort_by{|re| -re.sales_count }.take(limit)
+    end
+  end
 end
