a7d24208eeb5101cad5b482bffda6767c0864b96
siman-man <k128585@ie.u-ryukyu.ac.jp>
2014-05-08 09:36:15 +0900

    Bug Fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
-  validates :price, presence: true, numericality: { greater_than: 100 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 100 }
   validates :name, length: { minimum: 2, maximum: 100 }
 end
