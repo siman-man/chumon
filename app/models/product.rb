bee002eea55f0185134f588b70645449d0b7b134
siman-man <k128585@ie.u-ryukyu.ac.jp>
2014-05-08 11:51:42 +0900

    temp save

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   has_many :order_details
+  has_many :orders, through: :order_details
 
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 100 }
   validates :name, length: { minimum: 2, maximum: 100 }
